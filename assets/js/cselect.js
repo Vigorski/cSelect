6ed4de5cf6f468c867cecd7fe4884a416b5f860b
Igor Veleski <vele.igor@gmail.com>
2021-09-05 23:33:27 +0200

    add search query feature, fix search query 'next option' bug, fix focus between input and main select ele

--- assets/js/cselect.js
+++ assets/js/cselect.js
@@ -1,3 +1,21 @@
+// KEYCODES FOR KEYBOARD CHARACTERS
+const KEY_TAB = 9;
+const KEY_SHIFT = 16;
+const KEY_SPACE = 32;
+const KEY_ENTER = 13;
+const KEY_UP = 38;
+const KEY_DOWN = 40;
+const KEY_ESC = 27;
+
+// ALIAS FOR DROPDOWN ACTIONS
+const DROP_OPEN = 'add';
+const DROP_CLOSE = 'remove';
+const DROP_TOGGLE = 'toggle';
+
+// OPERANDS FOR SEARCHING FOR AVAILABLE OPTIONS
+const OPERAND_ADD = 'add';
+const OPERAND_SUBTRACT = 'subtract';
+
 export default class CSelect {
 	constructor(select, features) {
 		this.animated = features?.animated ?? true;
@@ -8,6 +26,7 @@ export default class CSelect {
 		this.cSelectSingle = document.createElement('div');
 		this.cSelectDrop = document.createElement('div');
 		this.cSelectSearch = this.search ? document.createElement('div') : null;
+		this.cSelectSearchInput = this.search ? document.createElement('input') : null;
 		this.cSelectResults = document.createElement('ul');
 
 		init(this);
@@ -35,7 +54,7 @@ export default class CSelect {
 		// remove selected from old
 		oldSelected.element.selected = false;
 		oldSelected.selected = false;
-		this.cSelectResults.querySelector('.selected').classList.remove('selected');
+		this.cSelectResults.querySelector('.selected')?.classList.remove('selected');
 
 		// add selected to new
 		newSelected.element.selected = true;
@@ -56,6 +75,7 @@ export default class CSelect {
 		switch (type) {
 			case DROP_TOGGLE:
 				this.cSelectDrop.style.height = isOpen ? 0 : animatedDropHeight;
+				this.search && !isOpen && this.cSelectSearchInput.focus();
 				break;
 			case DROP_CLOSE:
 				if (!isOpen) break;
@@ -71,23 +91,7 @@ export default class CSelect {
 	}
 }
 
-// KEYCODES FOR KEYBOARD CHARACTERS
-const KEY_SPACE = 32;
-const KEY_ENTER = 13;
-const KEY_UP = 38;
-const KEY_DOWN = 40;
-const KEY_ESC = 27;
-
-// ALIAS FOR DROPDOWN ACTIONS
-const DROP_OPEN = 'add';
-const DROP_CLOSE = 'remove';
-const DROP_TOGGLE = 'toggle';
-
-// OPERANDS FOR SEARCHING FOR AVAILABLE OPTIONS
-const OPERAND_ADD = 'add';
-const OPERAND_SUBTRACT = 'subtract';
-
-const init = (_this) => {
+function init(_this) {
 	const cSelectLabel = document.createElement('span');
 	const cSelectLabelArrow = document.createElement('div');
 
@@ -101,7 +105,7 @@ const init = (_this) => {
 	cSelectLabel.innerText = _this.selectedOption.label;
 
 	// Append children label, arrow + options
-	initOptions(_this);
+	initOptions(_this, _this.options);
 	_this.cSelectSingle.appendChild(cSelectLabel);
 	_this.cSelectSingle.appendChild(cSelectLabelArrow);
 	_this.cSelect.appendChild(_this.cSelectSingle);
@@ -110,26 +114,25 @@ const init = (_this) => {
 	_this.cSelect.appendChild(_this.cSelectDrop);
 	_this.select.after(_this.cSelect);
 	_this.select.style.display = 'none';
-};
+}
 
-const initSearch = (_this) => {
-	const cSelectSearchInput = document.createElement('input');
+function initSearch(_this) {
 	const cSelectSearchIcon = document.createElement('div');
 
 	// Add classes
 	_this.cSelectSearch.classList.add('cSelect__search');
-	cSelectSearchInput.classList.add('cSelect__search-input');
+	_this.cSelectSearchInput.classList.add('cSelect__search-input');
 	cSelectSearchIcon.classList.add('cSelect__search-icon');
-	cSelectSearchInput.tabIndex = 0;
+	_this.cSelectSearchInput.tabIndex = 0;
 
 	// Append children
-	_this.cSelectSearch.appendChild(cSelectSearchInput);
+	_this.cSelectSearch.appendChild(_this.cSelectSearchInput);
 	_this.cSelectSearch.appendChild(cSelectSearchIcon);
 	_this.cSelectDrop.appendChild(_this.cSelectSearch);
-};
+}
 
-const initOptions = (_this) => {
-	_this.options.forEach((item) => {
+function initOptions(_this, options) {
+	options.forEach((item) => {
 		if (item.hidden) return;
 		const option = document.createElement('li');
 		option.classList.add('cSelect__option');
@@ -147,42 +150,43 @@ const initOptions = (_this) => {
 			_this.toggleDropdown(DROP_CLOSE);
 		});
 	});
-};
+}
 
-const getFormattedOptions = (options) => {
+function getFormattedOptions(options) {
 	return [...options].map((optionEle) => {
 		return {
-			value: optionEle.value,
-			label: optionEle.label,
-			selected: optionEle.selected,
-			disabled: optionEle.disabled,
+			value: optionEle.value ?? '',
+			label: optionEle.label ?? '',
+			selected: optionEle.selected ?? false,
+			disabled: optionEle.disabled ?? false,
 			hidden: false,
-			element: optionEle,
+			element: optionEle ?? null,
 		};
 	});
-};
+}
 
-const addEvents = (_this) => {
-	const searchInput = _this.cSelectSearch.querySelector('.cSelect__search-input');
-
+function addEvents(_this) {
 	_this.cSelectSingle.addEventListener('click', () => {
 		_this.toggleDropdown(DROP_TOGGLE);
+		clearSearchQuery(_this);
 	});
 
 	_this.cSelect.addEventListener('blur', () => {
 		setTimeout(() => {
 			// maybe use e.relatedTarget to avoid async
-			if (document.activeElement !== searchInput) {
+			if (document.activeElement !== _this.cSelectSearchInput) {
 				_this.toggleDropdown(DROP_CLOSE);
+				clearSearchQuery(_this);
 			}
 		}, 0);
 	});
 
-	searchInput.addEventListener('blur', () => {
+	_this.cSelectSearchInput.addEventListener('blur', () => {
 		setTimeout(() => {
 			// maybe use e.relatedTarget to avoid async
 			if (document.activeElement !== _this.cSelect) {
 				_this.toggleDropdown(DROP_CLOSE);
+				clearSearchQuery(_this);
 			}
 		}, 0);
 	});
@@ -200,35 +204,91 @@ const addEvents = (_this) => {
 				_this.toggleDropdown(DROP_OPEN);
 				break;
 			case KEY_SPACE:
-				if (document.activeElement === searchInput) break;
+				if (document.activeElement === _this.cSelectSearchInput) break;
 				_this.toggleDropdown(DROP_TOGGLE);
 				break;
 			case KEY_ENTER:
 			case KEY_ESC:
 				_this.toggleDropdown(DROP_CLOSE);
 				break;
+			case KEY_TAB:
+			case KEY_SHIFT:
+				break;
 			default:
-				searchQuery(_this, searchInput);
+				searchQuery(_this, _this.cSelectSearchInput);
 				break;
 		}
 	});
-};
+
+	_this.cSelectSearchInput.addEventListener('keydown', (e) => {
+		switch (e.keyCode) {
+			case KEY_ENTER:
+				e.preventDefault();
+				clearSearchQuery(_this);
+				break;
+			default:
+				break;
+		}
+	});
+}
 
 function getAvailableOption(_this, operand) {
 	const currentIndex = _this.selectedOptionIndex;
-	let prevIndex = null;
+	let availableIndex = null;
 
 	for (let i = 1; i < _this.options.length; i++) {
-		if (operand === OPERAND_ADD) prevIndex = _this.options[currentIndex + i];
-		if (operand === OPERAND_SUBTRACT) prevIndex = _this.options[currentIndex - i];
-		if (prevIndex?.disabled || prevIndex?.hidden) continue;
-		return prevIndex;
+		if (operand === OPERAND_ADD) availableIndex = _this.options[currentIndex + i];
+		if (operand === OPERAND_SUBTRACT) availableIndex = _this.options[currentIndex - i];
+		if (availableIndex?.disabled || availableIndex?.hidden) continue;
+		return availableIndex;
 	}
 }
 
-const searchQuery = (_this, searchInput) => {
-	const results = [];
+function searchQuery(_this, searchInput) {
+	const formatedInputText = searchInput.value.toLowerCase();
+	const results = _this.options.filter((item) => {
+		const formatedOptionText = item.label.toLowerCase();
+		return formatedOptionText.indexOf(formatedInputText) > -1;
+	});
 
-	const ins = _this.options[1].label.indexOf(searchInput.value);
-	console.log(ins);
-};
+	if (results.length === 0) {
+		results.push({
+			value: '',
+			label: 'No results match your query',
+			selected: false,
+			disabled: true,
+			hidden: false,
+			element: null,
+		});
+	}
+
+	hideResults(_this);
+	showResults(_this, results);
+}
+
+function clearSearchQuery(_this) {
+	const searchInput = _this.cSelectSearchInput;
+
+	if (_this.search && searchInput.value.length > 0) {
+		searchInput.value = '';
+		searchQuery(_this, searchInput);
+	}
+}
+
+function showResults(_this, results) {
+	for (let option of results) {
+		option.hidden = false;
+	}
+	
+	initOptions(_this, results);
+}
+
+function hideResults(_this) {
+	for (let option of _this.options) {
+		option.hidden = true;
+	}
+
+	while (_this.cSelectResults.firstChild) {
+		_this.cSelectResults.removeChild(_this.cSelectResults.lastChild);
+	}
+}
